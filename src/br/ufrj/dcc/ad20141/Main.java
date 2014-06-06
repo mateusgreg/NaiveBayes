30866fb1278f60b620d50685b7f7860a69465490
IanCastro <ian_c_m@hotmail.com>
2014-06-06 19:14:47 -0300

    calculando para mais de 1 feature.
    calcula para todas as features, e para as 10 melhores individuais.
    usando o log para evitar underflow.

--- src/br/ufrj/dcc/ad20141/Main.java
+++ src/br/ufrj/dcc/ad20141/Main.java
@@ -10,20 +10,15 @@ import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class Main {
 
-	private static final int FEATURES = 58;
+	private static final int FEATURES = 57;
 	private static final int INSTANCES = 4601;
 	private static final int SPAM = 1813;
 	private static final int NSPAM = INSTANCES - SPAM;
+	private static final double DELTA = 0.1;
+	private static final double percentSpam = 1.0*SPAM/INSTANCES;
+	private static final double percentNSpam = 1.0*NSPAM/INSTANCES;
 
-	/**
-	 * @param args
-	 */
-	/**
-	 * @param args
-	 */
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 
 		try {
@@ -35,129 +30,112 @@ public class Main {
 
 			int lin = 0;
 			while ((nextLine = reader.readNext()) != null) { 	
-
 				for (int col=0; col<FEATURES; col++){
 					spambase[lin][col] = Double.parseDouble(nextLine[col]);
 				}
-
-				//System.out.println(spambase[lin][0] + " " + spambase[lin][1] + " etc.\n");
 				lin++;
 			}
 
 			reader.close();
 
-			/*double meanEstimator = 0;
-			double varianceEstimator = 0;
-			int numberSpamsTrainningSet = (int)(SPAM * 0.8);
-
-			for (int i=0; i < numberSpamsTrainningSet; i++) {
-				meanEstimator += spambase[i][0];
-			}
-			meanEstimator /= numberSpamsTrainningSet;
-
-			for (int i=0; i < numberSpamsTrainningSet; i++) {
-				varianceEstimator += (spambase[i][0] - meanEstimator) * (spambase[i][0] - meanEstimator);
-			}
-			varianceEstimator /= numberSpamsTrainningSet;
-
-			double stdDeviation = Math.sqrt(varianceEstimator);
-
-			//NormalDistribution normal = new NormalDistribution(0, 1);
-			NormalDistribution normal = new NormalDistribution(meanEstimator, stdDeviation);
-			*/
 			NormalDistribution[] normalSpam = new NormalDistribution[FEATURES];
 			NormalDistribution[] normalNSpam = new NormalDistribution[FEATURES];
-			double percentSpam = 0.5;
-			double percentNSpam = 0.5;
 			for(int feature = 0; feature<FEATURES; feature++){
 				int numberSpamsTrainningSet = (int)(SPAM * 0.8);
 				normalSpam[feature] = calculaNormal(spambase, feature, 0, numberSpamsTrainningSet);
 				int numberNSpamsTrainningSet = (int)(NSPAM * 0.8);
-				normalNSpam[feature] = calculaNormal(spambase, feature, SPAM, numberNSpamsTrainningSet);
+				normalNSpam[feature] = calculaNormal(spambase, feature, SPAM, SPAM + numberNSpamsTrainningSet);
 			}
+			
+			
 			Double [] percentHit = new Double[FEATURES];
 			for(int feature = 0; feature<FEATURES; feature++){
 				System.out.println("feature: " + feature);
-				normalSpam[feature].density(0);
 			    int countErrosSpam = 0;
 			    for (int i=0; i < SPAM; i++) {
-			    	if(normalSpam[feature].density(spambase[i][feature])*percentSpam < 
-			    			normalNSpam[feature].density(spambase[i][feature])*percentNSpam){
+			    	if(normalSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA)*percentSpam < 
+			    			normalNSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA)*percentNSpam){
 			    		countErrosSpam++;
 			    	}
 				}
 			    int countErrosNSpam = 0;
 			    for (int i=SPAM; i < SPAM + NSPAM; i++) {
-			    	if(normalSpam[feature].density(spambase[i][feature])*percentSpam > 
-			    			normalNSpam[feature].density(spambase[i][feature])*percentNSpam){
+			    	if(normalSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA)*percentSpam > 
+			    			normalNSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA)*percentNSpam){
 			    		countErrosNSpam++;
 			    	}
 				}
-//			    System.out.println("percentual erro nao spam: " + 1.0*countErrosNSpam/NSPAM);
-//			    System.out.println("percentual erro spam: " + 1.0*countErrosSpam/SPAM);
-//			    System.out.println("percentual erro total: " + 1.0*(countErrosSpam + countErrosNSpam)/INSTANCES);
 			    percentHit[feature] = (1.0 - 1.0*(countErrosSpam + countErrosNSpam)/INSTANCES);
+			    System.out.println("percentual acerto spam: " + (1.0 - 1.0*countErrosSpam/SPAM));
+			    System.out.println("percentual acerto nSpam: " + (1.0 - 1.0*countErrosNSpam/NSPAM));
 			    System.out.println("percentual acerto: " + percentHit[feature]);
 			    System.out.println();
 			}
-			int numFeature = 10;
-			for(int feature = 0; feature < FEATURES-numFeature+1; feature++){
-				System.out.println("feature: " + feature);
-			    int countErrosSpam = 0; 
-			    for (int i=0; i < SPAM; i++) {
-			    	double mulNormalSpam = percentSpam;
-			    	double mulNormalNSpam = percentNSpam;
-			    	for (int j = 0; j<numFeature; j++){
-			    		mulNormalSpam *= normalSpam[feature].density(spambase[i][feature+j]);
-			    		mulNormalNSpam *= normalNSpam[feature].density(spambase[i][feature+j]);
-			    	}
-			    	if(mulNormalSpam < mulNormalNSpam){
-			    		countErrosSpam++;
-			    	}
-				}
-			    int countErrosNSpam = 0; 
-			    for (int i=SPAM; i < SPAM + NSPAM; i++) {
-			    	int mulNormalSpam = 1;
-			    	int mulNormalNSpam = 1;
-			    	for (int j = 0; j<numFeature; j++){
-			    		mulNormalSpam *= normalSpam[feature].density(spambase[i][feature+j]);
-			    		mulNormalNSpam *= normalNSpam[feature].density(spambase[i][feature+j]);
-			    	}
-			    	if(mulNormalSpam > mulNormalNSpam){
-			    		countErrosSpam++;
-			    	}
-			    	/*if(normalSpam[feature].density(spambase[i][feature])*normalSpam[feature].density(spambase[i][feature]) > 
-			    	normalNSpam[feature].density(spambase[i][feature])*normalNSpam[feature].density(spambase[i][feature])){
-			    		countErrosNSpam++;
-			    	}*/
-				}
-			    System.out.println("percentual erro nao spam: " + 1.0*countErrosNSpam/NSPAM);
-			    System.out.println("percentual erro spam: " + 1.0*countErrosSpam/SPAM);
-			    System.out.println("percentual acerto: " + (1.0 - 1.0*(countErrosSpam + countErrosNSpam)/INSTANCES));
-			    System.out.println();
+		    System.out.println();
+		    System.out.println();
+			
+
+		    int countErrosSpam = 0;
+		    int[] features = {2,4,6,15,20,22,23,51,52,55};
+			System.out.println("features: {2,4,6,15,20,22,23,51,52,55}");
+		    for (int i=0; i < SPAM; i++) {
+		    	double sumNormalSpam = Math.log(percentSpam);
+		    	double sumNormalNSpam = Math.log(percentNSpam);
+		    	for(int j = 0; j < features.length; j++){
+		    		sumNormalSpam += Math.log(normalSpam[features[j]].probability(spambase[i][features[j]] - DELTA, spambase[i][features[j]] + DELTA));
+    				sumNormalNSpam += Math.log(normalNSpam[features[j]].probability(spambase[i][features[j]] - DELTA, spambase[i][features[j]] + DELTA));
+		    	}
+		    	if(sumNormalSpam <= sumNormalNSpam){
+		    		countErrosSpam++;
+		    	}
 			}
-/*
-			System.out.println("FEATURE: word_freq_make\n");
+		    int countErrosNSpam = 0;
+		    for (int i=SPAM; i < SPAM + NSPAM; i++) {
+		    	double sumNormalSpam = Math.log(percentSpam);
+		    	double sumNormalNSpam = Math.log(percentNSpam);
+		    	for(int j = 0; j < features.length; j++){
+		    		sumNormalSpam += Math.log(normalSpam[features[j]].probability(spambase[i][features[j]] - DELTA, spambase[i][features[j]] + DELTA));
+    				sumNormalNSpam += Math.log(normalNSpam[features[j]].probability(spambase[i][features[j]] - DELTA, spambase[i][features[j]] + DELTA));
+		    	}
+		    	if(sumNormalSpam >= sumNormalNSpam){
+		    		countErrosNSpam++;
+		    	}
+			}
+		    System.out.println("percentual acerto spam: " + (1.0 - 1.0*countErrosSpam/SPAM));
+		    System.out.println("percentual acerto nSpam: " + (1.0 - 1.0*countErrosNSpam/NSPAM));
+		    System.out.println("percentual acerto: " + (1.0 - 1.0*(countErrosSpam + countErrosNSpam)/INSTANCES));
+		    System.out.println();
+		    System.out.println();
+			
 
-			System.out.println("===============================");
-			System.out.println("TRAINNING SET: 80% OF SPAM BASE");
-			System.out.println("===============================\n");
-			System.out.println("SPAMS OF TRAINNING SET: 1 a " + numberSpamsTrainningSet + "\n");
-
-			System.out.println("Média:         " + normalSpam.getMean());
-			System.out.println("Variância:     " + normalSpam.getStandardDeviation()*normalSpam.getStandardDeviation());//varianceEstimator);
-			System.out.println("Desvio Padrão: " + normalSpam.getStandardDeviation() + "\n");
-
-			System.out.println("===============================================");
-			System.out.println("TESTING SET: PDF EVALUATED FOR 20% OF SPAM BASE");
-			System.out.println("===============================================\n");
-			System.out.println("SPAMS OF TESTING SET: " + (numberSpamsTrainningSet+1) + " a " + SPAM + "\n");
-
-			//for (int i=numberSpamsTrainningSet; i < SPAM; i++) {
-			//	System.out.println("Email " + (i+1) + ": " + normal.density(spambase[i][0]));
-			//}
-*/
-
+		    countErrosSpam = 0;
+		    System.out.println("Todas features.");
+		    for (int i=0; i < SPAM; i++) {
+		    	double sumNormalSpam = Math.log(percentSpam);
+		    	double sumNormalNSpam = Math.log(percentNSpam);
+		    	for(int feature = 0; feature < FEATURES; feature++){
+		    		sumNormalSpam += Math.log(normalSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA));
+    				sumNormalNSpam += Math.log(normalNSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA));
+		    	}
+		    	if(sumNormalSpam <= sumNormalNSpam){
+		    		countErrosSpam++;
+		    	}
+			}
+		    countErrosNSpam = 0;
+		    for (int i=SPAM; i < SPAM + NSPAM; i++) {
+		    	double sumNormalSpam = Math.log(percentSpam);
+		    	double sumNormalNSpam = Math.log(percentNSpam);
+		    	for(int feature = 0; feature < FEATURES; feature++){
+		    		sumNormalSpam += Math.log(normalSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA));
+    				sumNormalNSpam += Math.log(normalNSpam[feature].probability(spambase[i][feature] - DELTA, spambase[i][feature] + DELTA));
+		    	}
+		    	if(sumNormalSpam >= sumNormalNSpam){
+		    		countErrosNSpam++;
+		    	}
+			}
+		    System.out.println("percentual acerto spam: " + (1.0 - 1.0*countErrosSpam/SPAM));
+		    System.out.println("percentual acerto nSpam: " + (1.0 - 1.0*countErrosNSpam/NSPAM));
+		    System.out.println("percentual acerto: " + (1.0 - 1.0*(countErrosSpam + countErrosNSpam)/INSTANCES));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -167,7 +145,6 @@ public class Main {
 	public static NormalDistribution calculaNormal(Double [][] spambase, int feature, int inicial, int numberTrainningSet){
 		double meanEstimator = 0;
 		double varianceEstimator = 0;
-		//int numberSpamsTrainningSet = (int)(SPAM * 0.8);
 
 		for (int i=inicial; i < numberTrainningSet; i++) {
 			meanEstimator += spambase[i][feature];
@@ -181,7 +158,6 @@ public class Main {
 
 		double stdDeviation = Math.sqrt(varianceEstimator);
 
-		//NormalDistribution normal = new NormalDistribution(0, 1);
 		if(stdDeviation == 0) stdDeviation += 0.0001;
 		return new NormalDistribution(meanEstimator, stdDeviation);	
 	}
